36ab3afdfc0b0230fe925aaf97726aa4d06f6561
unknown <dev.cslee@gmail.com>
2019-07-23 09:19:50 +0900

    first commit

---
+++ project/settings.py
@@ -0,0 +1,13 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DEBUG = True
+
+DATABASE = {
+    'postgre': {
+        'name': '',
+        'passwd': '',
+        
+    }
+}
